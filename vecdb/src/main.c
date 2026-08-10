e7e3cc005075a8c960b982a0721df51db96a72ca
BidhanMainali <BidhanMainali@hotmail.com>
2026-08-10 15:57:27 -0700

    vecdb: add 'embed' command to inspect vectors (M1)

--- vecdb/src/main.c
+++ vecdb/src/main.c
@@ -1,5 +1,7 @@
 #include "store.h"
+#include "embed.h"
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -15,6 +17,7 @@ static void usage(FILE *out, const char *prog) {
         "                            (dim defaults to %u)\n"
         "  stats  <file.vdb>         print information about a store\n"
         "  open   <file.vdb>         check that a store opens and is valid\n"
+        "  embed  \"<text>\" [dim]      embed text and print a vector summary\n"
         "  help                      show this message\n"
         "  version                   show the version\n",
         prog, VDB_DEFAULT_DIM);
@@ -81,6 +84,63 @@ static int cmd_open(int argc, char **argv) {
     return 0;
 }
 
+static int cmd_embed(int argc, char **argv) {
+    const uint32_t PRINT_CAP = 20;
+    const char    *text;
+    uint32_t       dim = VDB_DEFAULT_DIM;
+    float         *vec;
+    uint32_t       i;
+    uint32_t       nonzero = 0;
+    uint32_t       printed = 0;
+    double         norm = 0.0;
+
+    if (argc < 1) {
+        fprintf(stderr, "vecdb embed: missing text argument\n");
+        return 1;
+    }
+    text = argv[0];
+
+    if (argc >= 2) {
+        long v = strtol(argv[1], NULL, 10);
+        if (v <= 0) {
+            fprintf(stderr, "vecdb embed: dimension must be a positive integer\n");
+            return 1;
+        }
+        dim = (uint32_t)v;
+    }
+
+    vec = malloc((size_t)dim * sizeof(float));
+    if (vec == NULL) {
+        fprintf(stderr, "vecdb embed: out of memory\n");
+        return 1;
+    }
+
+    embed_tf(text, vec, dim, 0);
+
+    for (i = 0; i < dim; i++) {
+        if (vec[i] != 0.0f) {
+            nonzero++;
+        }
+        norm += (double)vec[i] * (double)vec[i];
+    }
+
+    printf("Text:     \"%s\"\n", text);
+    printf("Dim:      %u\n", dim);
+    printf("Nonzero:  %u\n", nonzero);
+    printf("L2 norm:  %.6f\n", sqrt(norm));
+    printf("Buckets (up to %u shown):\n", PRINT_CAP);
+
+    for (i = 0; i < dim && printed < PRINT_CAP; i++) {
+        if (vec[i] != 0.0f) {
+            printf("  [%u] %.6f\n", i, vec[i]);
+            printed++;
+        }
+    }
+
+    free(vec);
+    return 0;
+}
+
 int main(int argc, char **argv) {
     const char *cmd;
 
@@ -99,6 +159,9 @@ int main(int argc, char **argv) {
     }
     if (strcmp(cmd, "open") == 0) {
         return cmd_open(argc - 2, argv + 2);
+    }
+    if (strcmp(cmd, "embed") == 0) {
+        return cmd_embed(argc - 2, argv + 2);
     }
     if (strcmp(cmd, "help") == 0) {
         usage(stdout, argv[0]);
